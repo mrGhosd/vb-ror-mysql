66d0b4e6680c0f32d7952d3a09ae00aabf1e3f3f
mrGhosd <vforvad@gmail.com>
2014-06-29 15:02:00 +0400

    deposit pages imposition is complete (i guess so...)

--- app/assets/javascripts/deposit.js
+++ app/assets/javascripts/deposit.js
@@ -9,7 +9,27 @@ $(document).ready(function()
             step: 250000,
             slide: function( event, ui ) {
                 $( ".deposit_amount_value" ).val(ui.value + " р." );
+                calculate_full_summ();
+                deposit_main_diagram_change();
+            },
+//            start: function(event, ui)
+//            {
+//                calculate_full_summ();
+////                deposit_main_diagram_change();
+//
+//            },
+            change: function(event, ui)
+            {
+                calculate_full_summ();
+                deposit_main_diagram_change();
+
             }
+//            stop: function(event, ui)
+//            {
+//                calculate_full_summ();
+//                deposit_main_diagram_change();
+//
+//            }
         });
     $("#slider_deposit_time").slider(
         {
@@ -18,8 +38,21 @@ $(document).ready(function()
             max: 36,
             value: 18,
             step: 1,
+            start: function()
+            {
+                calculate_full_summ();
+            },
             slide: function( event, ui ) {
-                $( ".deposit_time_value" ).val(ui.value + " м." );
+                deposit_time_info_value(ui.value);
+                calculate_full_summ();
+            },
+            change: function()
+            {
+                calculate_full_summ();
+            },
+            stop: function()
+            {
+                calculate_full_summ();
             }
         });
     $("#percents_slider").slider(
@@ -49,6 +82,11 @@ $(document).ready(function()
             }
         });
 
+    //Инициализация данных о депозите
+    var month = getTime();
+    $("span.month_count").text(month);
+    calculate_full_summ();
+
     $(".left_side").click(function()
     {
         if($(this).hasClass('active'))
@@ -63,6 +101,7 @@ $(document).ready(function()
             $('.right_side').removeClass('active');
             $('.left_side').addClass('active');
         }
+        calculate_full_summ();
     });
     $('.right_side').click(function()
     {
@@ -78,5 +117,80 @@ $(document).ready(function()
             $('.left_side').removeClass('active');
             $('.right_side').addClass('active');
         }
+        calculate_full_summ();
     });
 });
+
+
+function deposit_time_info_value(value)
+{
+    $( ".deposit_time_value" ).val(value + " м." );
+    $("span.month_count").text(value);
+}
+
+function getPercent()
+{
+    var percent;
+    if($(".main_percent_slider .left_side").hasClass('active'))
+    {
+        percent = 15;
+    }
+    else
+    {
+        percent = 30;
+    }
+    return percent;
+}
+
+function getAmount()
+{
+    var amount = $("#slider_deposit_amount").slider("value");
+    return amount;
+}
+function getTime()
+{
+    var time = $("#slider_deposit_time").slider("value");
+    return time;
+}
+
+
+function calculate_full_summ()
+{
+    var percent = getPercent();
+    var amount = getAmount();
+    var time = getTime();
+    var percents = (amount * percent/100) * time;
+
+    var result_sum = amount + ((amount * percent/100) * time);
+    $(".deposit_information .current_summ").text(parseInt(result_sum, 10) + " р.");
+    $(".deposit_information .user_deposit").text(amount + " р.");
+    $(".deposit_information .added_percents").text(percents + " р.");
+    $(".deposit_info_user p.user_amount").text(amount + " р.")
+    $(".deposit_info_user p.user_full_summ").text(result_sum + " р.");
+    deposit_main_diagram_change();
+}
+
+function deposit_main_diagram_change()
+{
+    var slider = $("#slider_deposit_amount");
+    var slider_time = $("#slider_deposit_time");
+    var current_height = 80;
+    var current_top_value = 34;
+    var profit_diagram_height = 40;
+    var profit_diagram_top_margin = 69;
+    var slider_current_value = slider.slider("value");
+    var time_current_value = slider_time.slider("value");
+    var slider_step = slider.slider("option", "step");
+    var exp = slider_current_value / slider_step;
+    var time_diff = slider_current_value / time_current_value;
+
+
+    $(".deposit_diagram_middle .deposit_diagram_middle_middle").animate({ "height": current_height + (exp * 3)}, 'slow');
+    $(".deposit_diagram_top").animate({"top": + current_top_value + (exp/4)});
+    $(".deposit_profit_diagram_middle .deposit_diagram_middle_middle")
+        .animate({ "height": profit_diagram_height + (exp * 1.5)}, 'slow');
+    $(".deposit_profit_diagram_top").animate({"top": + profit_diagram_top_margin + (exp/4)});
+
+
+
+}
