87128a165cacee57862035606e752051bc8cad95
mrGhosd <vforvad@gmail.com>
2014-10-26 17:49:25 +0300

    some changes

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -94,24 +94,4 @@ describe StocksController do
       delete :destroy, id: @stock_1
     end
   end
-
-  # describe 'GET #admin' do                #Использовать метод match_array
-  #   it 'populates of array of all stocks'
-  #   it 'rednders the :admin view'
-  # end
-  #
-  # describe 'GET #show' do
-  #   it 'assigns the requested stock to @stock' do
-  #     let(:stock) do
-  #       create(:stock)
-  #     end
-  #     stock = create(:stock)
-  #     get :show, id: stock
-  #     expect(assigns(:stock)).to eq stock
-  #   end
-  #   it 'renders the :show view'
-  # end
-
-
-  #......И так далее......ф
 end
