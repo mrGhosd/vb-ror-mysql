366b1bb391866d76e898549cfe88f2294b512f54
mrGhosd <vforvad@gmail.com>
2014-11-07 14:14:33 +0300

    add a model spec for deposit

---
+++ spec/models/deposit_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Deposit do
+  let!(:user) { create :user }
+  let!(:percent) { create :percent, :deposit }
+  let!(:confirmed_deposit) { create :deposit, :confirmed, user_id: user.id, percent_id: percent.id }
+  let!(:contribution_account) { create :contribution_account, :removed, deposit_id: confirmed_deposit.id }
+  let!(:unconfirmed_deposit) { create :deposit, :unconfirmed, user_id: user.id, percent_id: percent.id}
+
+  it "show contribution accounts to deposits" do
+    expect(confirmed_deposit.contribution_account).to eq([contribution_account])
+  end
+
+  it "return day differenece between now and created at attribute" do
+    expect(confirmed_deposit.days_diff).to eq((Time.zone.now.to_date - confirmed_deposit.created_at.to_date ).to_i)
+  end
+end
